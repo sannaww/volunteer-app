08d2d13e3ca9c35519de3cdee5c2baaab84e2f59
emm <emiliya.kobzeva@gmail.com>
2026-02-07 16:46:25 +0300

    eee

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,6 +7,5 @@ const authMiddleware = require('../middlewares/auth.middleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/me', authController.getMe);
-const authMiddleware = require('../middlewares/auth.middleware');
 
 module.exports = router;
