c7463f7d0def9815e5bc23ff3d056cc5f08134f8
emm <emiliya.kobzeva@gmail.com>
2026-02-08 13:45:16 +0300

    eeeyty

--- backend/services/auth-service/server.js
+++ backend/services/auth-service/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+const authRoutes = require('./routes/auth.routes');
+
+app.use('/', authRoutes);
 
 app.listen(5001, () => {
   console.log('Auth Service running on port 5001');
